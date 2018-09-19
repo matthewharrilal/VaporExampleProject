6ffcbe86f24191432c7e9aae6656de24db0f37be
Matthew Harrilal <matthewharrilal@gmail.com>
2018-09-19 14:04:27 -0700

    Added basic routing for the server

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,10 +1,17 @@
 import Vapor
+import Console
+
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+//    router.get("hello") { req in
+//        return "Hello Dapper Dan!"
+//    }
+    
+    router.get("goodbye") { req in
+    
+        return "This is the last of my departures."
     }
 
     // Example of configuring a controller
